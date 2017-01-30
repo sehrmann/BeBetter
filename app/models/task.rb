f8a6ac5365c4d50868d46f77608a574dd9acb228
Scott Ehrmann <s.s.ehrmann@gmail.com>
2017-01-30 11:48:35 -0500

    migration and model

---
+++ app/models/task.rb
@@ -0,0 +1,29 @@
+class Task < ApplicationRecord
+
+  def frequency(days_in_month)
+    case period
+      when "Day"
+        reps / days_in_month
+      when "Week"
+        reps / (7 * days_in_month)
+      when "Month"
+        reps
+      else
+        0
+    end
+  end
+
+  validates :name,
+    presence: true
+  validates :importance,
+    inclusion: { in: ["Very Low", "Low", "Medium", "High", "Very High"]}
+  validates :value,
+    presence: true,
+    numericality: true
+  validates :reps,
+    presence: true,
+    numericality: true
+  validates :period,
+    presence: true,
+    inclusion: { in: ["Day","Week","Month"]}
+end
