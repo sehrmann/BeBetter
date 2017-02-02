75c6bbb35e603de420b842c204f013cd236020f6
Scott Ehrmann <s.s.ehrmann@gmail.com>
2017-02-02 15:59:17 -0500

    fixed task unit test

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -25,24 +25,33 @@ describe Task, type: :model do
   it { should_not have_valid(:period).when(nil,'',5,'Year') }
 
   describe "instance methods" do
+    let(:harry) { User.new(
+      email: "hpotter@hogwarts.wiz",
+      name: "basiliskSlayer",
+      oauth_uid: "1",
+      current_month: 7
+    )}
     let(:dishes) { Task.new(
       name: "Do dishes",
       importance: "Low",
       reps: 1,
-      period: "Day")
-    }
+      period: "Day",
+      user: harry
+    )}
     let(:gym) { Task.new(
       name: "Go to the gym",
       importance: "High",
       reps: 3.5,
-      period: "Week")
-    }
+      period: "Week",
+      user: harry
+    )}
     let(:read) { Task.new(
       name: "Read a book",
       importance: "Medium",
       reps: 1,
-      period: "Month")
-    }
+      period: "Month",
+      user: harry
+    )}
 
     describe "#frequency" do
       it "calculates the number of times a task should be completed per month" do
@@ -59,7 +68,7 @@ describe Task, type: :model do
         read.set_value!
 
         expect(dishes.value).to eq(73)
-        expect(gym.value).to eq(242.0)
+        expect(gym.value).to eq(242)
         expect(read.value).to eq(3000)
       end
     end
